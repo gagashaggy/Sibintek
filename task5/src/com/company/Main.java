c83ecb5e2a8dc9660f514145a1ac4d9be409a513
Gagashaggy <gagashaggy@inbox.ru>
2020-02-24 17:07:30 +0300

    task5 - комментарии

--- task5/src/com/company/Main.java
+++ task5/src/com/company/Main.java
@@ -21,7 +21,13 @@ public class Main {
         checkDatabaseContainsReferentKeys(referentKeys, databaseKeys);
     }
 
+    /**
+     * Проверить, что список ключей БД не содержит лишних ключей
+     * @param referentKeys - эталонный список
+     * @param databaseKeys - список ключей БД
+     */
     private static void checkExtraKeys(List<String> referentKeys, List<String> databaseKeys) {
+        // создаём временный список, чтобы не изменять список, переданный в метод
         List<String> temp = new ArrayList<>(databaseKeys);
         temp.removeAll(referentKeys);
         if (!temp.isEmpty()) {
@@ -31,7 +37,13 @@ public class Main {
         } else System.out.println("Список содержит только эталонные ключи");
     }
 
+    /**
+     * Проверить, что список ключей БД содержит все эталонные ключи
+     * @param referentKeys - эталонный список
+     * @param databaseKeys - список ключей БД
+     */
     private static void checkDatabaseContainsReferentKeys(List<String> referentKeys, List<String> databaseKeys) {
+        // создаём временный список, чтобы не изменять список, переданный в метод
         List<String> temp = new ArrayList<>(referentKeys);
         temp.removeAll(databaseKeys);
         if (!temp.isEmpty()) {
